1445ae34affb6b3bf914c51d96bb349d170c18a1
g173742 <g173742@g.unicamp.br>
2017-10-03 10:54:37 -0300

    Update Servico.java

--- Servico.java
+++ Servico.java
@@ -6,7 +6,7 @@ public class Servico {
 	private String descricao;
 	private Date dataCriacao;
 	private int duracao;
-	private List<Prestador> prestadores;
+	//private List<Prestador> prestadores;
 	
 	public long getCodServico() {
 		return codServico;
@@ -32,10 +32,10 @@ public class Servico {
 	public void setDuracao(int duracao) {
 		this.duracao = duracao;
 	}
-	public List<Prestador> getPrestadores() {
+	/*public List<Prestador> getPrestadores() {
 		return prestadores;
 	}
 	public void setPrestadores(List<Prestador> prestadores) {
 		this.prestadores = prestadores;
-	}
+	}*/
 }
