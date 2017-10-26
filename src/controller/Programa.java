30a0eb21c8b2d00000144a504992d6f144a732b5
Lais Campos <l177779@g.unicamp.br>
2017-10-26 20:03:00 -0200

    refactor controller package

--- src/controller/Programa.java
+++ src/controller/Programa.java
@@ -1,58 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Contem funcoes basicas utilizadas pelo metodo principal
  */
 package controller;
 
-import application.Cliente;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import application.Salao;
 
 /**
  *
  * @author guilherme
  */
 public class Programa {
-        
-    /**
-     * 
-     * @param nome 
-     */
-    public void start(String nome) throws ParseException{
-        Salao.setNome("Vicellis");
-        boasVindas();
-        
-        try{
-            Cliente cli = new Cliente("Guilherme","47577220876","gg.dossantos@hotmail.com","19982534663",'M',new SimpleDateFormat("dd/MM/yyyy").parse("11/08/1998"));        
-            Cliente cli2 = new Cliente("José","47577220876","gg.dossantos@hotmail.com","19982534663",'M',new SimpleDateFormat("dd/MM/yyyy").parse("11/08/1998"));
-            
-            System.out.println("Clientes cadastrados: \n");
-            System.out.println(cli.toString());
-            System.out.println(cli2.toString());
-            
-        }catch(ParseException ex){
-            Logger LOGGER = Logger.getLogger(Programa.class.getName());
-            LOGGER.log(Level.SEVERE, "Houve um erro ao cadastrar os clientes.");
-        }
-        
-        despedida();
-    }   
-    
     /**
      * Exibe mensagem de boas vindas
      */
-    private void boasVindas(){
-        System.out.println("********************* Bem vindo ao salão de beleza "+Salao.getNome()+"! *********************");
+    public void boasVindas(){
+        System.out.println("********************* Bem vindo ao salão de beleza " + Salao.getNome() + "! *********************");
     }
 
     /**
      * Exibe mensagem de despedida
      */
-    private void despedida(){
-        System.out.println("********************* O salão "+Salao.getNome()+" agradece, volte sempre! *********************");
+    public void despedida(){
+        System.out.println("********************* O salão " + Salao.getNome() + " agradece, volte sempre! *********************");
     }
-    
 }
